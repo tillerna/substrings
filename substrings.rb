2704d4fe5256b3b453c332657346294ed77193d7
Nate Tiller <tillerna@gmail.com>
2022-08-05 14:37:55 -0400

    Setup

---
+++ substrings.rb
@@ -0,0 +1,16 @@
+# expected output:
+#
+# > substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+
+def substrings
+
+
+
+
+
+
+substrings("below", dictionary)
